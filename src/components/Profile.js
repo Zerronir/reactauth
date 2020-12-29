fce99c662dd930c351dc5a940fca8e4a4a44db8d
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-12-29 13:36:37 +0100

    Cambios en profile.js para comprobar el estado de autenticación del usuario

---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {useAuth0} from "@auth0/auth0-react";
+import JSONPretty from "react-json-pretty";
+import styles from './css/Profile.Module.css';
+
+
+const LoginButton = () => {
+    const { user, isAuthenticated}  = useAuth0();
+
+    // Comprobamos el estado de autenticación del usuario
+    if (isAuthenticated) {
+        return(
+            <div>
+                <div className={styles.userInfo}>
+                    <h2>Hola, {user.given_name}</h2>
+
+                    <img src={user.picture} alt={user.name} />
+                </div>
+                <p>{user.email}</p>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                Log in please to see your profile
+            </div>
+        )
+    }
+}
+
+export default LoginButton;
